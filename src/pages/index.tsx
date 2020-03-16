9afa3f7034795dfe7eb52f6f1963195efc9d03f3
kuzar <kuzar.k@pm.me>
2020-03-16 04:42:59 +0200

    changing the index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
+// import { Link } from 'gatsby';
 import Layout from '../layout';
 import './indexPage.scss';
 
@@ -7,9 +7,10 @@ class IndexPage extends React.PureComponent<{}, {}> {
   public render() {
     return (
       <Layout>
-        <h1>Hi There</h1>
-        <p>This is a attempt to make a personal site with static Gatsby</p>
-        <Link to="/page-2/">Go to page 2</Link>
+        <h1>Coming Soon!</h1>
+        <p>This site still under construction ,</p>
+        <p>Using AWS serverless + Django for backend and Gatsby + React + TS stack for Frontend.</p>
+        {/*<Link to="/page-2/">Go to page 2</Link>*/}
       </Layout>
     );
   }
